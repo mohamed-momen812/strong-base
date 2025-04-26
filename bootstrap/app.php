4d2349e7f5d13fe423fde4042ab6419fe69589c2
Mohamed Momen <mohamedmomen81296@gmail.com>
2025-04-26 23:38:30 +0300

    creating role and permission system to be continue .....

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\CheckPermission;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -29,7 +28,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
-            'permission' => CheckPermission::class,
         ]);
 
     })
